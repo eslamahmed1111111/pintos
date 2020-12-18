eff4d62cea2016710fc77cf156e9681866f0b818
eslamahmed1111111 <45874665+eslamahmed1111111@users.noreply.github.com>
2020-12-18 21:20:21 +0200

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -142,5 +142,6 @@ int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
+bool changing_priority (const struct list_elem *elem_a, const struct list_elem *elem_b, void *aux ) ;
 bool wakeup_thread(const struct list_elem *, const struct list_elem *, void *);
 #endif /* threads/thread.h */
